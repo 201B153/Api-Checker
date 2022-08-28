07d6faf2e06f310de54881e185873d72fe8fe7a6
201b153 <tomarm698@gmail.com>
2022-08-28 22:21:57 +0530

    Finishing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Api-Checker
+
+- It Checks api's just like postman.
--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector(
   '[data-request-headers]'
 );
-const responseHeadersContainer = document.querySelector('[data-response-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
+const responseHeadersContainer = document.querySelector(
+  '[data-response-headers]'
+);
 
 document
   .querySelector('[data-add-query-param-btn]')
@@ -27,70 +29,73 @@ document
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
-axios.interceptors.request.use(request => {
-    request.customData = request.customData || {}
-    request.customData.startTime = new Date().getTime()
-    return request
-})
-
-const { requestEditor, updateResponseEditor } = setupEditors()
-
-axios.interceptors.response.use(updateTime, e => {
-    return Promise.reject(updateEndTime(e.response))
-})
+axios.interceptors.request.use((request) => {
+  request.customData = request.customData || {};
+  request.customData.startTime = new Date().getTime();
+  return request;
+});
 
 function updateEndTime(response) {
-    response.customData = response.customData || {}
-    response.customData.time = new Date().getTime() - response.config.customData.startTime
-    return response
+  response.customData = response.customData || {};
+  response.customData.time =
+    new Date().getTime() - response.config.customData.startTime;
+  return response;
 }
 
+axios.interceptors.response.use(updateEndTime, (e) => {
+  return Promise.reject(updateEndTime(e.response));
+});
+
+const { requestEditor, updateResponseEditor } = setupEditors();
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  let data
+  let data;
   try {
-    data = JSON.parse(requestEditor.state.doc.toString() || null)
+    data = JSON.parse(requestEditor.state.doc.toString() || null);
   } catch (e) {
-    alert('JSON data is malformed')
-    return 
+    alert('JSON data is malformed');
+    return;
   }
 
   axios({
-    url: document.querySelector('[data-method]').value,
+    url: document.querySelector('[data-url]').value,
     method: document.querySelector('[data-method]').value,
     params: keyValuePairsToObjects(queryParamsContainer),
     headers: keyValuePairsToObjects(requestHeadersContainer),
     data,
   })
-  .catch(e => e)
-  .then((response) => {
-    document.querySelector('[data-response-section]').classList.remove('d-none')
-    updateResponseDetails(response)
-    updateResponseEditor(response.data)
-    updateResponseHeaders(response.headers)
-    console.log(response);
-  });
+    .catch((e) => e)
+    .then((response) => {
+      document
+        .querySelector('[data-response-section]')
+        .classList.remove('d-none');
+      updateResponseDetails(response);
+      updateResponseEditor(response.data);
+      updateResponseHeaders(response.headers);
+      console.log(response);
+    });
 });
 
 function updateResponseDetails(response) {
-    document.querySelector('[data-status]').textContent = response.status
-    document.querySelector('[data-time]').textContent = response.customData.time
-    document.querySelector('[data-size]').textContent = prettyBytes(
-        JSON.stringify(response.data).length + JSON.stringify(response.headers).length
-    )
+  document.querySelector('[data-status]').textContent = response.status;
+  document.querySelector('[data-time]').textContent = response.customData.time;
+  document.querySelector('[data-size]').textContent = prettyBytes(
+    JSON.stringify(response.data).length +
+      JSON.stringify(response.headers).length
+  );
 }
 
 function updateResponseHeaders(headers) {
-    responseHeadersContainer.innerHTML = ""
-    Object.entries(headers).forEach(([key, value]) => {
-        const keyElement = document.createElement('div')
-        keyElement.textContent = key
-        responseHeadersContainer.append(keyElement)
-        const valueElement = document.createElement('div')
-        valueElement.textContent = value
-        responseHeadersContainer.append(valueElement)
-    })
+  responseHeadersContainer.innerHTML = '';
+  Object.entries(headers).forEach(([key, value]) => {
+    const keyElement = document.createElement('div');
+    keyElement.textContent = key;
+    responseHeadersContainer.append(keyElement);
+    const valueElement = document.createElement('div');
+    valueElement.textContent = value;
+    responseHeadersContainer.append(valueElement);
+  });
 }
 
 function createKeyValuePair() {
